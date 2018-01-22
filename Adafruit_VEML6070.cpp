de74f151fe8387ccbb4b5acaaf4cf2a9452e51dd
dean <deanm1278@gmail.com>
2018-01-22 14:26:05 -0500

    DM: updated docs

--- Adafruit_VEML6070.cpp
+++ Adafruit_VEML6070.cpp
@@ -1,18 +1,32 @@
-/*************************************************** 
-  This is a library for the Adafruit VEML6070 UV Sensor Breakout
-
-  Designed specifically to work with the VEML6070 sensor from Adafruit
-  ----> https://www.adafruit.com/products/2899
-
-  These sensors use I2C to communicate, 2 pins are required to  
-  interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
+/*!
+ * @file Adafruit_VEML6070.cpp
+ *
+ * @mainpage Adafruit VEML6070 UV sensor
+ *
+ * @section intro_sec Introduction
+ *
+ * Designed specifically to work with the VEML6070 sensor from Adafruit
+ * ----> https://www.adafruit.com/products/2899
+ *
+ * These sensors use I2C to communicate, 2 pins (SCL+SDA) are required
+ * to interface with the breakout.
+ *
+ * Adafruit invests time and resources providing this open source code,
+ * please support Adafruit and open-source hardware by purchasing
+ * products from Adafruit!
+ *
+ * @section dependencies Dependencies
+ *
+ *
+ * @section author Author
+ *
+ * Written by Limor Fried/Ladyada for Adafruit Industries.
+ *
+ * @section license License
+ *
+ * BSD license, all text here must be included in any redistribution.
+ *
+ */
 
 #if (ARDUINO >= 100)
  #include "Arduino.h"
@@ -24,6 +38,12 @@
 #include "Adafruit_VEML6070.h"
 
 
+/**************************************************************************/
+/*! 
+    @brief  setup and initialize communication with the hardware
+    @param itime the integration time to use for the data
+*/
+/**************************************************************************/
 void Adafruit_VEML6070::begin(veml6070_integrationtime_t itime) {
   Wire.begin();
   Wire.beginTransmission(VEML6070_ADDR_L);
@@ -32,6 +52,13 @@ void Adafruit_VEML6070::begin(veml6070_integrationtime_t itime) {
   delay(500);
 }
 
+
+/**************************************************************************/
+/*! 
+    @brief  read the chips UV sensor
+    @return the UV reading as a 16 bit integer
+*/
+/**************************************************************************/
 uint16_t Adafruit_VEML6070::readUV() {
   if (Wire.requestFrom(VEML6070_ADDR_H, 1) != 1) return -1;
   uint16_t uvi = Wire.read();
--- Adafruit_VEML6070.h
+++ Adafruit_VEML6070.h
@@ -1,18 +1,21 @@
-/*************************************************** 
-  This is a library for the Adafruit VEML6070 UV Sensor Breakout
-
-  Designed specifically to work with the VEML6070 sensor from Adafruit
-  ----> https://www.adafruit.com/products/2899
-
-  These sensors use I2C to communicate, 2 pins are required to  
-  interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
+/*!
+ * @file Adafruit_VEML6070.h
+ *
+ * Designed specifically to work with the VEML6070 sensor from Adafruit
+ * ----> https://www.adafruit.com/products/2899
+ *
+ * These sensors use I2C to communicate, 2 pins (SCL+SDA) are required
+ * to interface with the breakout.
+ *
+ * Adafruit invests time and resources providing this open source code,
+ * please support Adafruit and open-source hardware by purchasing
+ * products from Adafruit!
+ *
+ * Written by Limor Fried/Ladyada for Adafruit Industries.  
+ *
+ * BSD license, all text here must be included in any redistribution.
+ *
+ */
 
 #if (ARDUINO >= 100)
  #include "Arduino.h"
@@ -22,10 +25,15 @@
 #include "Wire.h"
 
 // really unusual way of getting data, your read from two different addrs!
-#define VEML6070_ADDR_H 0x39
-#define VEML6070_ADDR_L 0x38
+#define VEML6070_ADDR_H 0x39 ///< High address
+#define VEML6070_ADDR_L 0x38 ///< Low address
 
-// three different integration times
+
+/**************************************************************************/
+/*! 
+    @brief  integration time definitions
+*/
+/**************************************************************************/
 typedef enum veml6070_integrationtime {
   VEML6070_HALF_T,
   VEML6070_1_T,
@@ -34,6 +42,11 @@ typedef enum veml6070_integrationtime {
 } veml6070_integrationtime_t;
 
 
+/**************************************************************************/
+/*! 
+    @brief  Class that stores state and functions for interacting with VEML6070 sensor IC
+*/
+/**************************************************************************/
 class Adafruit_VEML6070 {
  public:
   Adafruit_VEML6070() {};
